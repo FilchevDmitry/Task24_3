a5ecafb571d3eb7aff362e5d66c68a8686fd9a58
DmityFilchev <filchev_dmitry@mail.ru>
2022-10-30 16:01:27 +0300

    '30_10'

--- Task24_3/Task24_3.cpp
+++ Task24_3/Task24_3.cpp
@@ -8,19 +8,20 @@ int main()
 	std::time_t t = std::time(nullptr);
 	std::tm temp = *std::localtime(&t);
 	std::cin >> std::get_time(&temp, "%M:%S");
-	std::time_t out = std::mktime(&temp);
+	int timer = (temp.tm_min*60)+temp.tm_sec;
 	while (true)
 	{	
 		std::time_t start = std::time(nullptr);
 		std::time_t end= std::time(nullptr);
-		std::time_t delta = std::difftime(out, start);
+		std::time_t delta = (start+timer)-start;
 		std::tm tm = *std::localtime(&delta);
 		while (end<start+1)
 		{
 			end= std::time(nullptr);
 		}
 		std::cout << std::put_time(&tm, "%M:%S") << std::endl;
-		if (delta == 0) break;
+		timer--;
+		if (delta <= 0) break;
 	}
 	std::cout << "DING! DING! DING!" << std::endl;
 }
